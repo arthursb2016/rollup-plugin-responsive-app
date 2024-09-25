06a133725aab57ff0a7e184f794fb64ead94bd90
Arthur Borba <arthur@lnxtech.com.br>
2024-09-25 14:28:23 -0300

    adjusting

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,28 @@
 import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser'
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-    sourcemap: true
-  },
+  output: [
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    }
+  ],
   external: ['magic-string'],
-  plugins: [typescript()]
+  plugins: [
+    typescript(),
+    resolve(),
+    commonjs(),
+    terser()
+  ]
 }
